79d6a3e19b06c2b0974ba5fe4ba0cf66aaac7df2
Nanmanat Varisthanist <53960201+nanmanat@users.noreply.github.com>
2022-10-15 11:41:08 +0700

    first commit testing multi file function

---
+++ function.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void findSum(int num1, int num2)
+{
+    printf("%d\n", num1 + num2);
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "function.c"
+
+
+int main()
+{
+    findSum(1, 2);
+}
