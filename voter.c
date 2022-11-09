f8e7a8276e876d07b4c910399fd64c0d82d77a08
Latina789 <115864165+Latina789@users.noreply.github.com>
2022-11-09 23:30:22 +0700

    Edit voter

--- voter.c
+++ voter.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
 
+void poll();
 int voter_screen()
 {
-    return 1;
+    int option;
+    printf("Please select one of this option\n");
+    printf("1) View poll list\n");
+    printf("2) Exit\n\n");
+
+    printf("Enter your option: ");
+    scanf("%d", &option);
+
+    if (option == 1)
+    {
+        poll();
+    }
+    else if (option == 2)
+    {
+        printf("Thank you");
+    }
+
+    return 0;
+}
+void poll()
+{
+    FILE *poll_list;
+
+    poll_list = fopen("poll_list.txt", "r");
+
+    char poll[100];
+
+    int number = 1;
+    int poll_number;
+
+    printf("+==============================+\n");
+    printf("|    This is your poll list    |\n");
+    printf("+==============================+\n");
+
+    while (fgets(poll, 100, poll_list))
+    {
+        printf("%d) %s", number, poll);
+        number++;
+    }
+
+    number = 1;
+    fclose(poll_list);
 }
